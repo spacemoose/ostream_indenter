af03cf4bcc49b380ae1d85f31a895c39cba80e07
STG <stg@profidata.com>
2015-09-11 15:14:53 +0200

    basic implementation seems to be working

--- indent_facet.hpp
+++ indent_facet.hpp
@@ -4,15 +4,14 @@
 #include <algorithm>
 #include <iostream>
 #include <iomanip>
+#include <cassert>
 
 
 
-/// Create the indenter facet which is constant, and knows its indentation level.
-template <int ind_lev>
 class indent_facet : public std::codecvt<char, char, std::mbstate_t> {
 public:
 	explicit indent_facet( int indent_level, size_t ref = 0)
-		: std::codecvt<char, char, std::mbstate_t>(ref){}
+		: std::codecvt<char, char, std::mbstate_t>(ref), m_indentation_level(indent_level) {}
 	typedef std::codecvt_base::result result;
 	typedef std::codecvt<char, char, std::mbstate_t> parent;
 	typedef parent::intern_type intern_type;
@@ -22,72 +21,73 @@ public:
 	int &state(state_type &s) const { return *reinterpret_cast<int *>(&s); }
 
 protected:
-
-	// Override so the do_out() virtual function is called.
-	virtual bool do_always_noconv() const throw() override {
-		return ind_lev==0;
-	}
-
-
-	/// The override of do_out, which uses the indentation level.
-	/// Unfortunately, the debugger shows me that m_indentation_level is zero
-	/// no matter how often I have called push.
 	virtual result do_out(state_type &need_indentation,
 		const intern_type *from, const intern_type *from_end, const intern_type *&from_next,
 		extern_type *to, extern_type *to_end, extern_type *&to_next
-		) const
-	{
-		result res = std::codecvt_base::noconv;
-		for (; (from < from_end) && (to < to_end); ++from, ++to) {
-			// 0 indicates that the last character seen was a newline.
-			// thus we will print a tab before it. Ignore it the next
-			// character is also a newline
-			if ((state(need_indentation) == 0) && (*from != '\n')) {
-				res = std::codecvt_base::ok;
-				state(need_indentation) = 1;
-				for(int i=0; i<ind_lev; ++i){
-					*to = '\t'; ++to;
-				}
-				if (to == to_end) {
-					res = std::codecvt_base::partial;
-					break;
-				}
-			}
-			*to = *from; // Copy the next character.
+		) const override;
 
-			// If the character copied was a '\n' mark that state
-			if (*from == '\n') {
-				state(need_indentation) = 0;
+	// Override so the do_out() virtual function is called.
+	virtual bool do_always_noconv() const throw() override {
+		return m_indentation_level==0;
+	}
+	unsigned int m_indentation_level = 0;
+
+};
+
+indent_facet::result indent_facet::do_out(state_type &need_indentation,
+	const intern_type *from, const intern_type *from_end, const intern_type *&from_next,
+	extern_type *to, extern_type *to_end, extern_type *&to_next
+	) const
+{
+	result res = std::codecvt_base::noconv;
+	for (; (from < from_end) && (to < to_end); ++from, ++to) {
+		// 0 indicates that the last character seen was a newline.
+		// thus we will print a tab before it. Ignore it the next
+		// character is also a newline
+		if ((state(need_indentation) == 0) && (*from != '\n')) {
+			res = std::codecvt_base::ok;
+			state(need_indentation) = 1;
+			for(int i=0; i<m_indentation_level; ++i){
+				*to = '\t'; ++to;
+			}
+			if (to == to_end) {
+				res = std::codecvt_base::partial;
+				break;
 			}
 		}
+		*to = *from; // Copy the next character.
 
-		if (from != from_end) {
-			res = std::codecvt_base::partial;
+		// If the character copied was a '\n' mark that state
+		if (*from == '\n') {
+			state(need_indentation) = 0;
 		}
-		from_next = from;
-		to_next = to;
+	}
 
-		return res;
-	};
+	if (from != from_end) {
+		res = std::codecvt_base::partial;
+	}
+	from_next = from;
+	to_next = to;
 
+	return res;
 };
 
 
 
+/// I hate the way I solved this, but I can't think of a better way
+/// around the problem.  I even asked stackoverflow for help:
+///
+///   http://stackoverflow.com/questions/32480237/apply-a-facet-to-all-stream-output-use-custom-string-manipulators
+///
+///
+namespace  indent_manip{
 
-namespace indent_manip{
+static const int index = std::ios_base::xalloc();
 
-static const int index {  std::ios_base::xalloc()};
-
-/// Here I create a new instance of the indent_facet class.  The
-/// contructor is called, but but the imbued locale doesn't seem to
-/// use the new instance.
 static std::ostream & push(std::ostream& os)
 {
 	auto ilevel = ++os.iword(index);
-	std::ios_base::sync_with_stdio(false);
-	auto t = new indent_facet<2>;
-	os.imbue(std::locale(os.getloc(), t));
+	os.imbue(std::locale(os.getloc(), new indent_facet(ilevel)));
 	return os;
 }
 
@@ -98,20 +98,50 @@ std::ostream& pop(std::ostream& os)
 	return os;
 }
 
+/// Clears the ostream indentation set, but NOT the raii_guard.
 std::ostream& clear(std::ostream& os)
 {
-	os.iword(index)=0;
+	os.iword(index) = 0;
 	os.imbue(std::locale(os.getloc(), new indent_facet(0)));
 	return os;
 }
 
+
+
 /// Provides a RAII guard around your manipulation.
-class guard
+class raii_guard
 {
 public:
-	guard(std::ostream& os):m_oref(os){};
-	~guard(){clear(m_oref);}
+	raii_guard(std::ostream& os):
+		start_level(os.iword(index)),
+		oref(os)
+	{}
+
+	~raii_guard()
+	{
+		reset();
+	}
+
+	/// Resets the streams indentation level to the point itw as at
+	/// when the guard was created.
+	void reset()
+	{
+		oref.iword(index) = start_level;
+		oref.imbue(std::locale(oref.getloc(), new indent_facet(start_level)));
+	}
+
+
+
+
+/*	/// Set both the stream and the RAII guard to zero.
+	void clear()
+	{
+		oref.iword(index) = 0;
+		start_level=0;
+		}*/
 private:
-	std::ostream& m_oref;
+	std::ostream& oref;
+	 int start_level;
 };
+
 }
--- indent_test.cpp
+++ indent_test.cpp
@@ -7,30 +7,76 @@
 #include "indent_facet.hpp"
 
 
-void call_me()
+
+void demo()
 {
-	std::cout << "- Maybe you have output in a function somehwere, but the indentation\n"
-		"level is actually determined by the calling function.\n";
+	indent_manip::raii_guard rg(std::cout);
+	std::cout << "<DEMO START>\n"
+			  << indent_manip::push <<  "This is after a push... "
+			  << indent_manip::push <<"that last push\n"
+			  <<"only affects me on a new line of course."
+			  << indent_manip::pop << "\nPop has the expected effect.\n";
+	rg.reset();
+	std::cout << "raii_guard.reset()  brings me back to the original raii_guard state.\n"
+			  << "<DEMO STOP>\n";
 }
 
-// For why this is necessary, see:
-// http://stackoverflow.com/questions/26387054/how-can-i-use-stdimbue-to-set-the-locale-for-stdwcout
+void rai_demo()
+{
+	indent_manip::raii_guard rg(std::cout);
+	std::cout << "The safe indenter object just provides a RAII guard around identation.\n"
+			  << indent_manip::push
+			  << "If I forget to call reset, or an exception is thrown...\n";
+	throw std::runtime_error("oops");
+	std::cout << "The pop never gets called:" << indent_manip::pop;
+}
+
+
+
+void trivial()
+{
+	/// This probably has to be called once for every program:
+    // http://stackoverflow.com/questions/26387054/how-can-i-use-stdimbue-to-set-the-locale-for-stdwcout
+	std::ios_base::sync_with_stdio(false);
+	std::cout << "I want to push indentation levels:\n" << indent_manip::push
+			  << "To arbitrary depths\n" << indent_manip::push
+			  << "and pop them\n" << indent_manip::pop
+			  << "back down\n" << indent_manip::pop
+			  << "like this.\n" << indent_manip::pop;
+}
+
 int main()
 {
-	std::ios_base::sync_with_stdio(false);
-    std::cout.imbue(std::locale(std::cout.getloc(),new indent_facet(0)));
-	indent_manip::guard img(std::cout);	// This is a RAII gaurd to ensure
-										// clear() is called even if exceptio
-										// is called.
-	std::cout << "Why would you want to use indent manipulator?\n"
-			  << indent_manip::push;
+	trivial();
 
-	call_me();
-	std::cout << "- Okay, the single \t case is only a small saving, but...\n"
-			  << indent_manip::push
-			  << "+ imagine the benefits\n"
-			  << "for nested indents!\n" << indent_manip::clear;
-	std::cout<<"Note the indent_manipulator class uses RAII to guarentee that when this \n"
-			 <<"functions exits, the indent level is cleared\n";
+
+
+	indent_manip::raii_guard rg (std::cout);
+	std::cout << "Here I call demo starting at zero indentation level:\n\n";
+	demo();
+	std::cout << indent_manip::push
+			  << "\n\nNow within an indent level:\n";
+	demo();
+
+	std::cout << "Demo left the indentation level where it found it.\n"
+			  << indent_manip::clear << "Clear pust me at zero.\n";
+
+	try
+	{
+		std::cout << indent_manip::push << indent_manip::push
+				  << "\n\nThe rai guard, resets, it doesn't clear:\n";
+		rai_demo();
+	}
+	catch(std::runtime_error& e)
+	{
+		std::cerr << "\n=Cerr of course has its own indentation level=\n\n";
+		std::cout << "But for instructional purposes we see that the RAII guard retored\n"
+				  << "the original indent level.\n";
+
+	}
+	std::cout << indent_manip::clear
+			  << "I can also call clear to make sure I'm at zero at any particular point.\n";
+
+
 
 }
